f72003cc2abbca31f1e56100dc04baca3a456148
johan-sxs <johan.saicoos@gmail.com>
2021-07-14 16:25:39 -0300

    Update Bolillero.cs

--- Bolillero.Core/Bolillero.cs
+++ Bolillero.Core/Bolillero.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Linq;
 
 namespace Bolillero.Core
 {
     public class Bolillero
     {
-      
+     public void bolillas()
+     {
+
+     }
+
+       
+      public void Sacarunabolilla()
+     {
+
+     }
+     public void IngresarBolillas()
+     {
+
+     }
+     public void Jugarnveces()
+     {
+
+     }
     }
 }
