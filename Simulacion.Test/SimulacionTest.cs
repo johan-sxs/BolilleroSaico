9c2697c6dcca28a9cd88c0c705cd7284806b265c
johan-sxs <johan.saicoos@gmail.com>
2021-09-15 15:28:41 -0300

    simulacion

---
+++ Simulacion.Test/SimulacionTest.cs
@@ -0,0 +1,16 @@
+using Bolillero.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Simulacion.Test
+{
+    [TestClass]
+    public class SimulacionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
