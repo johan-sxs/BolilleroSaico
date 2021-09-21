28ac62c3a0521f799c32b3744cea128e2e3f75b4
johan-sxs <johan.saicoos@gmail.com>
2021-09-21 16:20:14 -0300

    simulacion

--- Simulacion.Test/SimulacionTest.cs
+++ Simulacion.Test/SimulacionTest.cs
@@ -7,9 +7,24 @@ namespace Simulacion.Test
     [TestClass]
     public class SimulacionTest
     {
+        Simulacion Simulacion {get; set;}
+
         [TestMethod]
-        public void TestMethod1()
+        public void SimultaneidadConTask()
         {
+            var bolillero = new Bolillero.Core.Bolillero(10);
+
+            Simulacion = new Simulacion();
+
+            var jugada  = new List<byte>{5};
+
+            long cantidadVeces = 1000000;
+            double esperado = cantidadVeces/10;
+            var ganadasSinHilos = Simulacion.simularSinHilos(bolillero, cantidadVeces, jugada);
+
+            var ganadasConHilos = Simulacion.simularConHilos(bolillero, cantidadVeces, jugada, 4);
+
+            Assert.AreEqual(ganadasConHilos/esperado, ganadasSinHilos/esperado, 0.1);
             
         }
     }
